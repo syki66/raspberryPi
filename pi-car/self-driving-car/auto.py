4b91b7248479f8ceefecbd9f368302220251cdcb
syki66 <syeonki66@gmail.com>
2018-07-28 05:22:12 +0900

    modify auto.py

--- pi-car/self-driving-car/auto.py
+++ pi-car/self-driving-car/auto.py
@@ -1,147 +1,106 @@
-import RPi.GPIO as gpio
+import RPi.GPIO as GPIO
 import time
 import sys
 import Tkinter as tk
-from sensor import distance
 import random
+import math
 
+def initpins():
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(8, GPIO.OUT)
+        GPIO.setup(10, GPIO.OUT)
+        GPIO.setup(11, GPIO.OUT)
+        GPIO.setup(12, GPIO.OUT)
+	#GPIO.setup(38, GPIO.OUT)
+	#GPIO.setup(37, GPIO.IN)
+def reverse(t):
+        initpins()
+        GPIO.output(8, False)
+        GPIO.output(10, True)
+        GPIO.output(11, True)
+        GPIO.output(12, False)
+        time.sleep(t)
+        GPIO.cleanup()
+def forward(t):
+        initpins()
+        GPIO.output(8, True)
+        GPIO.output(10, False)
+        GPIO.output(11, False)
+        GPIO.output(12, True)
+        time.sleep(t)
+        GPIO.cleanup()
 
+def left(t):
+    initpins()
+    GPIO.output(10, True)
+    GPIO.output(8, False)
+    GPIO.output(12, True)
+    GPIO.output(11, False)
+    time.sleep(t)
+    GPIO.cleanup()
 
-def init():
-    gpio.setmode(gpio.BOARD)
-    gpio.setup(7, gpio.OUT)
-    gpio.setup(11, gpio.OUT)
-    gpio.setup(13, gpio.OUT)
-    gpio.setup(15, gpio.OUT)
+def right(t):
+    initpins()
+    GPIO.output(10, False)
+    GPIO.output(8, True)
+    GPIO.output(12, False)
+    GPIO.output(11, True)
+    time.sleep(t)
+    GPIO.cleanup()
 
-def forward(tf):
-    gpio.output(7, True)
-    gpio.output(11, False)
-    gpio.output(15, True)
-    gpio.output(13, False)
-    time.sleep(tf)
-    gpio.cleanup()
+def distance():
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(38, GPIO.OUT)
+        GPIO.setup(37, GPIO.IN)
+	
+	GPIO.output(38, False)
+	#time.sleep(0.1)
+	
+	GPIO.output(38, True)
+	time.sleep(0.00001)
+	GPIO.output(38, False)
 
-def reverse(tf):
-    gpio.output(7, False)
-    gpio.output(11, True)
-    gpio.output(15, False)
-    gpio.output(13, True)
-    time.sleep(tf)
-    gpio.cleanup()
+	while GPIO.input(37) == False:
+		pass
+	start = time.time()
+	while GPIO.input(37) == True:
+		pass
+	stop = time.time()
+	distance = 17000*(stop-start)
+	print(str(distance)+"cm")
+	GPIO.cleanup()
+	return distance
 
-def turn_left(tf):
-    gpio.output(7, True)
-    gpio.output(11, False)
-    gpio.output(15, True)
-    gpio.output(13, True)
-    time.sleep(tf)
-    gpio.cleanup()
+def rendom():
+	i = random.randrange(1,4)
+	r = random.randrange(1,7)
+	if i == 1:
+		right(r*0.33)
+	elif i == 2:
+		left(r*0.33)
+	else:
+		reverse(r*0.15)
 
-def turn_right(tf):
-    gpio.output(7, True)
-    gpio.output(11, True)
-    gpio.output(15, True)
-    gpio.output(13, False)
-    time.sleep(tf)
-    gpio.cleanup()
+d=[0,0,0]
+i = 3
 
-def pivot_right(tf):
-    gpio.output(7, False)
-    gpio.output(11, True)
-    gpio.output(15, True)
-    gpio.output(13, False)
-    time.sleep(tf)
-    gpio.cleanup()
+while True:
+	forward(0.1)
+	k = random.randrange(1,18)
+	if k == 6:
+		right(random.randrange(1,4)*0.3)
+	elif k == 5:
+		left(random.randrange(1,4)*0.3)
 
-def pivot_left(tf):
-    gpio.output(7, True)
-    gpio.output(11, False)
-    gpio.output(15, False)
-    gpio.output(13, True)
-    time.sleep(tf)
-    gpio.cleanup()
-
-'''
-def key_input(event):
-    init()
-    print 'Key : ', event.char
-    key_press = event.char
-    sleep_time = 0.030
-     
-    if key_press.lower() == 'w':
-	forward(sleep_time)
-    elif key_press.lower() == 's':
-	reverse(sleep_time)
-    elif key_press.lower() == 'a':
-	turn_left(sleep_time)
-    elif key_press.lower() == 'd':
-	turn_right(sleep_time)
-    elif key_press.lower() == 'q':
-	pivot_left(sleep_time)
-    elif key_press.lower() == 'e':
-	pivot_right(sleep_time)
-    else:
-	pass
-
-    curDis = distance('cm')
-    print('curdis is',curDis)
-
-    if curDis < 20:
-	init()
-	reverse(2)
-
-
-command = tk.Tk()
-command.bind('<KeyPress>', key_input)
-command.mainloop()
-'''
-
-def check_front():
-    init()
-    dist = distance()
-
-    if dist < 15:
-	print('Too close,',dist)
-	init()
-	reverse(2)
-	dist = distance()
-        if dist < 15:
-	    print('Too close,',dist)
-	    init()
-	    pivot_left(3)
-	    init()
-	    reverse(2)
-	    dist = distance()
-	    if dist < 15:
-		print('Too close, giving up',dist)
-		sys.exit()
-
-
-def autonomy():
-    tf = 0.030
-    x = random.randrange(0,4)
-
-    if x == 0:
-	for y in range(30):
-	    check_front()
-	    init()
-	    forward(tf)
-    elif x == 1:
-	for y in range(30):
-	    check_front()
-	    init()
-	    pivot_left(tf)
-    elif x == 2:
-	for y in range(30):
-	    check_front()
-	    init()
-	    turn_right(tf)
-    elif x == 3:
-	for y in range(30):
-	    check_front()
-	    init()
-	    turn_left(tf)
-
-for z in range(10):
-    autonomy()
+	if distance() < 25 :
+		rendom()
+	if distance() > 500:
+		rendom()
+	
+	d.append(round(distance()))
+	print(d[i])
+	
+	if (d[i] == d[i-1] and d[i] == d[i-2]):
+		rendom()
+		rendom()
+	i=i+1
